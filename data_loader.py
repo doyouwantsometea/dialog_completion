6cefe168b33410367e3f162541f827d5cdac04bb
doyouwantsometea <e920542@gmail.com>
2024-08-02 12:13:15 +0200

    optionally removing following turn context

--- data_loader.py
+++ data_loader.py
@@ -11,22 +11,22 @@ class DataLoader(object):
     def __init__(self,
                  path: str,
                  role: str = 'Explainer',
-                 utterance_len: int = 100,
+                 turn_len: int = 100,
                  window: int = 2,
                  replace: bool = True):
         """
         Initialize DataLoader instance using a prompting configuration file.
         :param path: Path to the data file.
-        :param role: Speaker of the desired utterances (Explainer / Explainee).
-        :param utterance_len: Minimum token numbers of the desired utterances.
-        :param window: Number of prior and following utterances to be included in the dialogue segment.
-        :param replace: Whether to replace the filtered utterance with {missing part} placeholder.
+        :param role: Speaker of the desired turns (Explainer / Explainee).
+        :param turn_len: Minimum token numbers of the desired turns.
+        :param window: Number of prior and following turns to be included in the dialogue segment.
+        :param replace: Whether to replace the filtered turn with {missing part} placeholder.
         """
         super().__init__()
         # load prompting configuration:
         self.df = load_data_file(path)
         self.role = role
-        self.utterance_len = utterance_len
+        self.turn_len = turn_len
         self.window = window
         self.replace = replace
     
@@ -54,31 +54,36 @@ class DataLoader(object):
         level = self.df.loc[0, 'dialog_lvl']
         return mapping[level]
     
-    def filter_utternace(self) -> list:
+    def filter_turn(self) -> list:
         """
-        Filter utterances with the given conditions.
-        :return: List of indexes that point toward utterances in the dataframe.
+        Filter turns with the given conditions.
+        :return: List of indexes that point toward turns in the dataframe.
         """
-        df_filter = self.df[(self.df.role == self.role) & (self.df.turn_num_tokens > self.utterance_len)]
+        df_filter = self.df[(self.df.role == self.role) & (self.df.turn_num_tokens > self.turn_len)]
         return df_filter.index
     
     def parse_diaolgue(self,
-                       index: int) -> str:
+                       index: int,
+                       following_context: bool) -> str:
         """
-        Parse utterances into dialogue segment for prompting.
-        :param index: Index of the filtered utterance.
+        Parse turns into dialogue segment for prompting.
+        :param index: Index of the filtered turn.
+        :param following_context: Whether to incorporate turns occuring after the target turn.
         :return: Dialogue string that can be applied to the prompt.
         """
         start = index - self.window if self.window <= index else 0
-        end = index + 1 + self.window if index + 1 + self.window <= len(self.df) else len(self.df)
+        if following_context:
+            end = index + 1
+        else:
+            end = index + 1 + self.window if index + 1 + self.window <= len(self.df) else len(self.df)
         # print(self.df[start:end])
 
-        target_utterance = str()
+        target_turn = str()
         parsed_dialogue = str()
         
         for i, r in self.df[start:end].iterrows():
             if i == index:
-                target_utterance = ' '.join(r.turn).rstrip()
+                target_turn = ' '.join(r.turn).rstrip()
 
             parsed_dialogue += f'{r.role}:'
             if i == index and self.replace:
@@ -86,7 +91,7 @@ class DataLoader(object):
             else:
                 parsed_dialogue += ' ' + ' '.join(r.turn) + '\n'
 
-        return target_utterance, parsed_dialogue
+        return target_turn, parsed_dialogue
     
 
 
