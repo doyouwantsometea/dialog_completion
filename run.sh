95f778f2ef1c589a1458cf20a81c5a7faa806bec
doyouwantsometea <e920542@gmail.com>
2024-08-15 12:35:53 +0200

    open_end trial batch; put back llama 3

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ export PYTHONPATH="${PYTHONPATH}:${PWD}/src"
 
 model='Mistral-7B-Instruct-v0.3'
 
-python3 main.py -m "$model" -l 60 --local
-python3 main.py -m "$model" -l 60 --local --topic
-python3 main.py -m "$model" -l 60 --local --speakers
-python3 main.py -m "$model" -l 60 --local --topic --speakers
+python3 main.py -m "$model" -l 60 --local --open_end
+python3 main.py -m "$model" -l 60 --local --topic --open_end
+python3 main.py -m "$model" -l 60 --local --speakers --open_end
+python3 main.py -m "$model" -l 60 --local --topic --speakers --open_end
