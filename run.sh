4419430699c1a179c74b250d6bd7f647a654e672
doyouwantsometea <e920542@gmail.com>
2024-08-15 12:48:39 +0200

    revise argument

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 export PYTHONPATH="${PYTHONPATH}:${PWD}/src"
 
 model='Mistral-7B-Instruct-v0.3'
+dataset='WIRED'
 
-python3 main.py -m "$model" -l 60 --local --open_end
-python3 main.py -m "$model" -l 60 --local --topic --open_end
-python3 main.py -m "$model" -l 60 --local --speakers --open_end
-python3 main.py -m "$model" -l 60 --local --topic --speakers --open_end
+python3 main.py -d "$dataset" -m "$model" -l 60 --local --open_end
+python3 main.py -d "$dataset" -m "$model" -l 60 --local --topic --open_end
+python3 main.py -d "$dataset" -m "$model" -l 60 --local --speakers --open_end
+python3 main.py -d "$dataset" -m "$model" -l 60 --local --topic --speakers --open_end
