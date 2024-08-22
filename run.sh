88311f7461c109c87e6a8941f31b69d57a94ee4a
doyouwantsometea <e920542@gmail.com>
2024-08-22 17:09:56 +0200

    eli5 on mistral

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PYTHONPATH="${PYTHONPATH}:${PWD}/src"
 
-model='Meta-Llama-3.1-8B-Instruct'
+model='Mistral-7B-Instruct-v0.3'
 dataset='ELI5'
 
 python3 main.py -d "$dataset" -m "$model" -l 30 --local
