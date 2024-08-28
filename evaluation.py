1fb73970ff49c4ea4fd18510637408a79e1d8fc2
doyouwantsometea <e920542@gmail.com>
2024-08-28 09:28:35 +0200

    set checkpoint for decoding errors

--- evaluation.py
+++ evaluation.py
@@ -37,7 +37,10 @@ if __name__ == "__main__":
     for root, dirs, files in os.walk('data/results'):
         for file in files:
             
-            df = pd.read_json(os.path.join(root, file))
+            try:
+                df = pd.read_json(os.path.join(root, file))
+            except:
+                continue
             
             new_col = []
             if args.FED:
