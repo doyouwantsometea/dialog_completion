24f4ef16e1bb1e936564ae1f1975e48e333a9314
doyouwantsometea <e920542@gmail.com>
2025-09-05 10:44:39 +0200

    prepare run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 export PYTHONPATH="${PYTHONPATH}:${PWD}/src"
 
-model='Mistral-7B-Instruct-v0.3'
+for model in 'Mistral-7B-Instruct-v0.3' 'Meta-Llama-3.1-8B-Instruct' 'claude-3-haiku-20240307'; do
+# model='Mistral-7B-Instruct-v0.3'
+# model='Meta-Llama-3.1-8B-Instruct'
+# model='claude-3-haiku-20240307'
+    for dataset in 'WikiDialog' 'ELI5'; do
+    # dataset='WIRED'
+    # dataset='WikiDialog'
+    # dataset='ELI5'
+        echo "Running model=$model dataset=$dataset"
+        
+        python3 main.py -d "$dataset" -m "$model" -l 30 --local
+        python3 main.py -d "$dataset" -m "$model" -l 30 --local --open_end
+        python3 main.py -d "$dataset" -m "$model" -l 30 --local --topic --speakers
+        python3 main.py -d "$dataset" -m "$model" -l 30 --local --topic --speakers --open_end
 
-dataset='WIRED'
-# dataset='WikiDialog'
-# dataset='ELI5'
-
-python3 main.py -d "$dataset" -m "$model" -l 30 --local
-python3 main.py -d "$dataset" -m "$model" -l 30 --local --open_end
-python3 main.py -d "$dataset" -m "$model" -l 30 --local --topic --speakers
-python3 main.py -d "$dataset" -m "$model" -l 30 --local --topic --speakers --open_end
+    done
+done
